530ae581062c38401cfd15b8f7ea3ba17a5e96a6
chompusama <chadaporn.p@kkumai.com>
2019-09-29 18:32:47 +0700

    updated package.json

--- server.js
+++ server.js
@@ -11,12 +11,6 @@ console.log('listening port', port)
 
 
 
-// "fs": "0.0.1-security",
-// "fs-extra": "^8.1.0",
-// "handlebars": "^4.3.4",
-// "moment": "^2.24.0",
-// "mongoose": "^5.7.1",
-// "morgan": "^1.9.1",
-// "node-cron": "^2.0.3",
-// "path": "^0.12.7",
-// "pdf-poppler": "^0.2.1",
+
+
+//     "pdf-poppler": "^0.2.1"
