091c02136531f940d4ac55b7ec1a7e54a83dc6e5
chompusama <chadaporn.p@kkumai.com>
2019-09-29 17:21:57 +0700

    init project

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// define path of each routes
+// const genImgRoutes = require("./api/routes/genImgRoutes");
+
+mongoose.connect(
+  "mongodb://atbb.space:27017/babyKickDB",
+  function(err) {
+        if(err) throw err;
+        console.log('Connect to MongoDB atb successful!')
+    }
+);
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use(express.static('uploads'))          // makes 'uploads' folder to public
+
+
+// app.use("/gen/img", genImgRoutes);          // Routes which should handle requests
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+
+module.exports = app;
